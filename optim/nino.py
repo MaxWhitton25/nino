a921c929de16d5802b31d383c942580562681d28
Max Whitton <mmw243@cornell.edu>
2024-12-02 16:14:05 -0500

    k schedule

--- optim/nino.py
+++ optim/nino.py
@@ -205,7 +205,7 @@ class NiNo:
                     print('loss before NiNo step: %.4f' % loss.item(), flush=True)
             else:
                 loss = None
-
+            k = self._k_schedule[self.period_i]
             if k is None:
                 k = self.get_k()
             if self.verbose:
